65932d875875b50b0532b7b6fa1e4f7f97fcdbc2
bdot868 <brandoncarter100@gmail.com>
2017-04-24 14:17:59 -0700

    Added password digest and create new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,36 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    end
   end
 
   def edit
   end
 
-  def create
-  end
-
   def update
-  end
-
-  def new
   end
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :name, :email, :password, :city)
+  end
+
 end
