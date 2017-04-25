8954ba506f45ea603060ac80c1abc39cdc9f9bef
bdot868 <brandoncarter100@gmail.com>
2017-04-25 09:42:52 -0700

    got posts linked to its user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,15 +7,52 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
+
+    # u = Post.find post_params[:post][:user_id]
+    # p = Post.new
+    # p.title = params[:post][:title]
+    # p.body = params[:post][:body]
+    # p.user = u
+    # p.save
+    # redirect_to user_path(u.id)
+
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      redirect_to edit_post_path(@post)
+    end
   end
 
   def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:id, :user_id, :title, :body, :pic_url, :_destroy)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  accepts_nested_attributes_for :posts, allow_destroy: true
 end
